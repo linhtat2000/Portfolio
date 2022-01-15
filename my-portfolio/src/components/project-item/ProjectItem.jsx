68d05bd2f0927863dcdfe862b9cf5aa2a48c060c
xiaoling2000 <linh2000business@gmail.com>
2022-01-15 22:56:27 +0700

    add project item

--- my-portfolio/src/components/project-item/ProjectItem.jsx
+++ my-portfolio/src/components/project-item/ProjectItem.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material";
+import Button from "@mui/material/Button";
+
+import theme from "../../components/button/theme";
 import "./projectItem.scss";
 
 const ProjectItem = () => {
-  return <div>Some project here</div>;
+  return (
+    <div className="container">
+      <div className="thumbnail">
+        <img
+          src="https://4.bp.blogspot.com/-5prgkINShq8/XwOeaSvk-ZI/AAAAAAAACP0/OrDecE6xQ5AG1acwoURcjIcgGFdwj7x1ACLcBGAsYHQ/w640-h427/web-design-landing-page-template_52683-18357.jpg"
+          alt="project's thumbnail"
+        />
+        <div className="buttons">
+          <ThemeProvider theme={theme}>
+            <Button variant="contained" color="primary" className="button">
+              view demo
+            </Button>
+            <Button variant="contained" color="primary" className="button">
+              view detail
+            </Button>
+          </ThemeProvider>
+        </div>
+      </div>
+      <div className="title">Project #01</div>
+    </div>
+  );
 };
 
 export default ProjectItem;
