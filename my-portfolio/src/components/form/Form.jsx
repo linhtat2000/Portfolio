28158922bd09454eb9381a38008dd7928055f053
xiaoling2000 <linh2000business@gmail.com>
2022-01-18 21:21:22 +0700

    update form functionality

--- my-portfolio/src/components/form/Form.jsx
+++ my-portfolio/src/components/form/Form.jsx
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import TextField from "@mui/material/TextField";
-
-import "./form.scss";
-import theme from "../../theme-btn";
 import Button from "@mui/material/Button";
-import { ThemeProvider } from "@mui/material";
+import { Alert, AlertTitle, ThemeProvider } from "@mui/material";
+import emailjs from "emailjs-com";
+
+import theme from "../../theme-btn";
+import "./form.scss";
+import { useState } from "react";
 
 const Form = () => {
+  const [isSucceed, setIsSucceed] = useState(false);
+  const formRef = useRef();
+
+  const handleFeedback = (isSucceed) => {
+    isSucceed ? (
+      <Alert severity="success" className="feedback-msg">
+        <AlertTitle>Your message has been sent</AlertTitle>
+        I'll respone you as soon as possible :) Have a nice day!
+      </Alert>
+    ) : (
+      <Alert severity="error" className="feedback-msg">
+        <AlertTitle>Oops! Your message has not been sent :(</AlertTitle>
+        Please try again later! Or you can directly hit me up on the phone
+      </Alert>
+    );
+  };
+  // const action = setTimeout(handleFeedback(isSucceed), 2000);
+  // clearTimeout(action);
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ewb244t",
+        "template_zn7rrws",
+        formRef.current,
+        "user_gx20gqVa47JHmhaGxjb5h"
+      )
+      .then(
+        (response) => {
+          console.log(response.status, response.text);
+          setIsSucceed(true);
+        },
+        (error) => {
+          console.log(error);
+          setIsSucceed(false);
+        }
+      );
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <form className="form">
+      <form id="contact-form" ref={formRef} onSubmit={handleSubmitForm}>
         <TextField
           fullWidth
           id="name-input"
@@ -18,6 +61,7 @@ const Form = () => {
           variant="filled"
           size="small"
           required
+          name="user_name"
           className="input"
           color="primary"
         />
@@ -28,6 +72,7 @@ const Form = () => {
           variant="filled"
           size="small"
           required
+          name="user_email"
           className="input"
           color="primary"
         />
@@ -37,6 +82,18 @@ const Form = () => {
           label="Phone Number"
           variant="filled"
           size="small"
+          name="user_phoneNumber"
+          className="input"
+          color="primary"
+        />
+        <TextField
+          fullWidth
+          id="subject-input"
+          label="Your Subject"
+          variant="filled"
+          size="small"
+          required
+          name="user_subject"
           className="input"
           color="primary"
         />
@@ -44,18 +101,25 @@ const Form = () => {
           fullWidth
           id="message"
           label="Leave me a message"
-          rows={5}
+          rows={4}
           variant="filled"
           size="small"
           multiline
           required
+          name="message"
           className="input"
           color="primary"
         />
-        <Button variant="contained" color="primary" className="custom-btn">
-          get in touch
+        <Button
+          variant="contained"
+          color="primary"
+          className="custom-btn"
+          type="submit"
+        >
+          send message
         </Button>
       </form>
+      ;
     </ThemeProvider>
   );
 };
